89434a162bceb20fb6492e113562cc67298fd879
Bryan <bryan.dov.bergman@gmail.com>
2021-03-25 23:08:01 +0200

    building menu

--- app/src/main/java/com/example/task_timer/MainActivity.java
+++ app/src/main/java/com/example/task_timer/MainActivity.java
@@ -28,20 +28,48 @@ public class MainActivity extends AppCompatActivity {
         ContentResolver contentResolver = getContentResolver();
 
         ContentValues values = new ContentValues();
-        values.put(TasksContract.Columns.TASKS_SORTORDER, "99");
-        values.put(TasksContract.Columns.TASKS_DESCRIPTION, "Completed");
-        String selection = TasksContract.Columns.TASKS_SORTORDER + " = " + 2;
-        int count = contentResolver.update(TasksContract.CONTENT_URI, values, selection, null);
-        Log.d(TAG, "onCreate: " + count + " record(s) updated");
-//        values.put(TasksContract.Columns.TASKS_NAME, "Content Provider");
-//        values.put(TasksContract.Columns.TASKS_DESCRIPTION, "Record content provider video");
-//        int count = contentResolver.update(TasksContract.buildTaskUri(4), values, null, null);
+
+
+//        values.put(TasksContract.Columns.TASKS_DESCRIPTION, "For deletion");
+//        String selection = TasksContract.Columns.TASKS_SORTORDER + " = ?";
+//
+//        String[] args = {"99"};
+//
+//        int count = contentResolver.update(TasksContract.CONTENT_URI, values, selection, null);
 //        Log.d(TAG, "onCreate: " + count + " record(s) updated");
 
-//        values.put(TasksContract.Columns.TASKS_NAME, "New Task 1");
-//        values.put(TasksContract.Columns.TASKS_DESCRIPTION, "Description 1");
+//        values.put(TasksContract.Columns.TASKS_SORTORDER, "99");
+//        values.put(TasksContract.Columns.TASKS_DESCRIPTION, "Completed");
+//        String selection = TasksContract.Columns.TASKS_SORTORDER + " = " + 2;
+//        int count = contentResolver.update(TasksContract.CONTENT_URI, values, selection, null);
+//        Log.d(TAG, "onCreate: " + count + " record(s) updated");
+
+//        values.put(TasksContract.Columns.TASKS_NAME, "Content Provider");
+//        values.put(TasksContract.Columns.TASKS_DESCRIPTION, "Record content provider video");
 //        values.put(TasksContract.Columns.TASKS_SORTORDER, 2);
+//        int count = contentResolver.update(TasksContract.buildTaskUri(2), values, null, null);
+//        Log.d(TAG, "onCreate: " + count + " record(s) updated");
+//
+//        values.put(TasksContract.Columns.TASKS_SORTORDER, 3);
+//        count = contentResolver.update(TasksContract.buildTaskUri(3), values, null, null);
+//        Log.d(TAG, "onCreate: " + count + " record(s) updated");
+
+//        values.put(TasksContract.Columns.TASKS_NAME, "New Task 2");
+//        values.put(TasksContract.Columns.TASKS_DESCRIPTION, "Description 2");
+//        values.put(TasksContract.Columns.TASKS_SORTORDER, 3);
 //        Uri uri = contentResolver.insert(TasksContract.CONTENT_URI, values);
+//
+//        values.put(TasksContract.Columns.TASKS_NAME, "New Task 4");
+//        values.put(TasksContract.Columns.TASKS_DESCRIPTION, "Description 4");
+//        values.put(TasksContract.Columns.TASKS_SORTORDER, 4);
+//        Uri uri = contentResolver.insert(TasksContract.CONTENT_URI, values);
+
+//        int count = contentResolver.delete(TasksContract.buildTaskUri(3), null, null);
+
+        String selection = TasksContract.Columns.TASKS_DESCRIPTION + " = ?";
+        String[] args = { "For deletion"};
+        int count = contentResolver.delete(TasksContract.CONTENT_URI, selection, args);
+        Log.d(TAG, "onCreate: " + count + " records(s) deleted");
 
 //        Cursor cursor = contentResolver.query(TasksContract.buildTaskUri(2),
         Cursor cursor = contentResolver.query(TasksContract.CONTENT_URI,
